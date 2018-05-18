f5b9c8c74bafada38935484cfa591bb79a735f38
CesarCedanoV <CesarCedanoV@gmail.com>
2018-05-17 23:08:16 -0400

    Finished the Lecture: Passing functions as Arguments so we learned that we can pass functions as arguments and this practice helps us to re-use the code and also our code will looks clean too.

--- The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
+++ The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
@@ -58,7 +58,8 @@ var cesar = Object.create(personProto,
 */
 
 
-// Primitives vs Objects
+/////////////////////////////
+// Lecture: Primitives vs Objects
 /*
 // Primitives
 var x = 6
@@ -104,3 +105,39 @@ change(age,obj);
 console.log(age)
 console.log(obj.city);
 */
+
+
+/////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990,1994,1995,2000,2015];
+
+function arrayCalc(arr, fn){
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++){
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes
+}
+
+function calculateAge(el) {
+  return 2018 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el){
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  }else {
+    return -1;
+  }
+}
+var ages = arrayCalc(years,calculateAge);
+var fullAges = arrayCalc(ages,isFullAge);
+var rates = arrayCalc(ages,maxHeartRate);
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
