cb014ebc9b6de06c2280a21ebe3098b5bf0f1039
CesarCedanoV <CesarCedanoV@gmail.com>
2018-05-18 00:02:13 -0400

    Completed Lecture: Immediately Invoked Function Expressions (IIFE), on this lecture, we learn about this new kind of function that javscripts looks like an expression. And this is useful for data privacy.

--- The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
+++ The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
@@ -172,3 +172,24 @@ designerQuestion('Abimael');
 
 interviewQuestion('Doctor')('Arlenys');
 */
+
+
+/////////////////////////////
+// Lecture: Immediately Invoked Function Expressions (IIFE)
+/*function game(){
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+}
+game();
+(function (){
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+//console.log(score);
+
+(function (goodLuck){
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
+*/
