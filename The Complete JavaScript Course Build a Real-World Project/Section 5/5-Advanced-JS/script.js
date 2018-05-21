48553515f6d8bec1af3419a9332466a6251b0dd6
CesarCedanoV <CesarCedanoV@gmail.com>
2018-05-21 14:47:13 -0400

    Added part of the code challenge from step 1 - 7

--- The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
+++ The Complete JavaScript Course Build a Real-World Project/Section 5/5-Advanced-JS/script.js
@@ -193,3 +193,190 @@ game();
 })(5);
 
 */
+/////////////////////////////
+// Lecture: Closures
+/*
+function retirement(retirementAge){
+  var a = ' years left until retirement.';
+  return function(yearOfBirth){
+    var age = 2018 - yearOfBirth;
+    console.log((retirementAge-age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(70);
+var retirementRepDom = retirement(65);
+
+
+retirementUS(1990);
+retirementGermany(2000);
+retirementRepDom(1994);
+
+retirement(60)(1994);
+
+
+function interviewQuestion(job) {
+  return function (name) {
+    if (job == 'designer') {
+        console.log(name + ', can you please explain what UX design is?');
+    }else if (job == 'developer') {
+      console.log(name + ', which one is your main programming language?');
+    } else {
+        console.log('Hello ' +name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion('developer')('Cesar');
+*/
+
+/////////////////////////////
+// Lecture: Bind, call and apply
+/*
+var person1 = {
+  name:'Cesar',
+  age:23,
+  job:'Developer',
+  presentation: function(style,timeOfDay) {
+    if (style == 'formal'){
+      console.log('Good '+timeOfDay+', Ladies and gentlemen! I\'m '+this.name+' and i\'m '+this.age+' years old. And also i work as a '+this.job+'.');
+    }else if(style == 'friendly'){
+      console.log('Weoo! What\'s up? I\'m '+this.name+', i\'m a '+this.job+' and i\'m '+this.age+' years old. Have a nice '+timeOfDay+'.');
+    }
+  }
+}
+
+var person2 = {
+  name:'Arlenys',
+  age:21,
+  job:'Doctor'
+}
+
+
+var person3 = {
+  name:'Abimael',
+  age:24,
+  job:'Front-End'
+}
+
+ 
+person1.presentation('friendly','Morning');
+
+person1.presentation.call(person2,'formal','afternoon');
+
+// person1.presentation.apply(person3,['formal','night']);
+
+var person3Formal = person1.presentation.bind(person3,'formal','morning');
+person3Formal();
+
+
+
+var years = [1990,1994,1995,2000,2015];
+
+function arrayCalc(arr, fn){
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++){
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes
+}
+
+function calculateAge(el) {
+  return 2018 - el;
+}
+
+function isFullAge(limit,el) {
+  return el >= limit;
+}
+
+var ages = arrayCalc(years,calculateAge);
+
+var fullRepDom = arrayCalc(ages,isFullAge.bind(this,20));
+console.log(ages);
+console.log(fullRepDom);
+*/
+
+
+/////////////////////////////
+// Code Challenge
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function(){
+  function Question(title,answers,correctAnswer){
+    this.title = title;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  };
+  
+  Question.prototype.displayQuestion = function(){
+    console.log(this.title);
+    for (let i=0; i < this.answers.length; i++){
+      console.log(i + ' - ' + this.answers[i]);
+    }
+  };
+  
+  Question.prototype.checkAnswerr = function(answer){
+    if (answer == this.correctAnswer){
+      console.log('Correct answer!');
+    }else {
+      console.log('Incorrect answer, try again.');
+    }
+  }
+  
+  
+  let questions = [new Question('Which is the largest country in the world?'
+                    ,['United States of America', 'Russia','Canada']
+                    ,1),
+                  new Question('What is the largest planet in our solar system?'
+                    ,['Jupiter','Earth','Sun']
+                    ,0),
+                  new Question('2 + 2 / 2?'
+                    ,['2', '4','3']
+                    ,2),
+                  new Question('What\'s my name?'
+                    ,['Cesar', 'Abimael','Arlenys']
+                    ,0)
+                  ];
+  
+  let randomIndex = Math.floor(Math.random() * questions.length);
+  questions[randomIndex].displayQuestion();
+  let selectedAnswer = parseInt(prompt('Please select the correct answer'));
+  questions[randomIndex].checkAnswerr(selectedAnswer);
+})();
+
+
